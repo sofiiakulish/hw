959862619454b2950ca6b321fffb5bc6bc197fb2
S.Kulish <S.Kulish@petrosoftinc.com>
2022-07-25 23:13:40 +0200

    hw06_pipeline_execution

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,29 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	for i := 0; i < len(stages); i++ {
+		select {
+		case <-done:
+			return in
+		default:
+			in = stages[i](in)
+		}
+	}
+
+	out := make(Bi)
+	go func() {
+		defer close(out)
+		for {
+			select {
+			case <-done:
+				return
+			case v, ok := <-in:
+				if ok == false {
+					return
+				}
+				out <- v
+			}
+		}
+	}()
+	return out
 }
