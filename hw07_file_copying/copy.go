0d947494d9099a8b1c04e9ff9bcb298542ab5d7d
S.Kulish <S.Kulish@petrosoftinc.com>
2022-09-03 12:24:51 +0200

    HW7 is completed

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,14 +2,68 @@ package main
 
 import (
 	"errors"
+	"github.com/cheggaaa/pb"
+	"io"
+	"os"
 )
 
 var (
 	ErrUnsupportedFile       = errors.New("unsupported file")
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
+	ErrTheSameFile           = errors.New("you are trying to rewrite the source file")
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	if fromPath == toPath {
+		return ErrTheSameFile
+	}
+
+	inFile, err := os.Open(fromPath)
+
+	if err != nil {
+		return err
+	}
+
+	fileStat, err := os.Stat(fromPath)
+	if err != nil {
+		return err
+	}
+
+	fileSize := fileStat.Size()
+	if fileSize < offset {
+		return ErrOffsetExceedsFileSize
+	}
+
+	defer inFile.Close()
+
+	if limit == 0 {
+		limit = fileSize
+	}
+
+	if limit > fileSize-offset {
+		limit = fileSize - offset
+	}
+
+	outFile, err := os.Create(toPath)
+
+	if err != nil {
+		return err
+	}
+
+	defer outFile.Close()
+
+	if offset > 0 {
+		inFile.Seek(offset, io.SeekStart)
+	}
+
+	bar := pb.StartNew(int(limit))
+	bar.Start()
+
+	reader := bar.NewProxyReader(inFile)
+
+	io.CopyN(outFile, reader, limit)
+
+	bar.Finish()
+
 	return nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,53 @@
 package main
 
-import "testing"
+import (
+	"crypto/md5"
+	"fmt"
+	"github.com/stretchr/testify/require"
+	"io"
+	"os"
+	"testing"
+)
+
+func FileMD5(path string) string {
+	h := md5.New()
+	f, err := os.Open(path)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	_, err = io.Copy(h, f)
+	if err != nil {
+		panic(err)
+	}
+	return fmt.Sprintf("%x", h.Sum(nil))
+}
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	err := Copy("testdata/file_does_not_exist.txt", "/tmp/copy_offset0_limit0.txt", 0, 0)
+	require.Error(t, err)
+
+	err = Copy("testdata/input.txt", "testdata/input.txt", 0, 0)
+	require.Error(t, err)
+
+	err = Copy("testdata/input.txt", "/tmp/offset_is_bigger_then_file_size.txt", 100000, 0)
+	require.Error(t, err)
+
+	Copy("testdata/input.txt", "/tmp/copy_offset0_limit0.txt", 0, 0)
+	require.Equal(t, FileMD5("testdata/out_offset0_limit0.txt"), FileMD5("/tmp/copy_offset0_limit0.txt"))
+
+	Copy("testdata/input.txt", "/tmp/copy_offset0_limit10.txt", 0, 10)
+	require.Equal(t, FileMD5("testdata/out_offset0_limit10.txt"), FileMD5("/tmp/copy_offset0_limit10.txt"))
+
+	Copy("testdata/input.txt", "/tmp/copy_offset0_limit1000.txt", 0, 1000)
+	require.Equal(t, FileMD5("testdata/out_offset0_limit1000.txt"), FileMD5("/tmp/copy_offset0_limit1000.txt"))
+
+	Copy("testdata/input.txt", "/tmp/copy_offset0_limit10000.txt", 0, 10000)
+	require.Equal(t, FileMD5("testdata/out_offset0_limit10000.txt"), FileMD5("/tmp/copy_offset0_limit10000.txt"))
+
+	Copy("testdata/input.txt", "/tmp/copy_offset100_limit1000.txt", 100, 1000)
+	require.Equal(t, FileMD5("testdata/out_offset100_limit1000.txt"), FileMD5("/tmp/copy_offset100_limit1000.txt"))
+
+	Copy("testdata/input.txt", "/tmp/copy_offset6000_limit1000.txt", 6000, 1000)
+	require.Equal(t, FileMD5("testdata/out_offset6000_limit1000.txt"), FileMD5("/tmp/copy_offset6000_limit1000.txt"))
 }
