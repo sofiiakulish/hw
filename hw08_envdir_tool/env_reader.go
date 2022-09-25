2c3143b80a37a24b69932d74c5feabdbcce1266c
S.Kulish <S.Kulish@petrosoftinc.com>
2022-09-25 16:14:33 +0200

    hw08_envdir_tool

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"io/fs"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+const INVALID_FILE_NAME_CHARACTER = "="
+
 type Environment map[string]EnvValue
 
 // EnvValue helps to distinguish between empty files and files with the first empty line.
@@ -11,6 +22,74 @@ type EnvValue struct {
 // ReadDir reads a specified directory and returns map of env variables.
 // Variables represented as files where filename is name of variable, file first line is a value.
 func ReadDir(dir string) (Environment, error) {
-	// Place your code here
-	return nil, nil
+	var valName string
+	var valValue string
+	var needRemove bool
+	var ok bool
+
+	result := make(Environment)
+
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		return result, fmt.Errorf("read dir: %w", err)
+	}
+
+	for _, f := range files {
+
+		valName, ok = getFileName(f)
+		if ok == false {
+			continue
+		}
+
+		valValue, needRemove, err = getValue(dir, f)
+		if err != nil {
+			return result, fmt.Errorf("get value error: %w", err)
+		}
+
+		result[valName] = EnvValue{valValue, needRemove}
+	}
+
+	return result, nil
+}
+
+func getFileName(file fs.FileInfo) (string, bool) {
+	if file.IsDir() {
+		return "", false
+	}
+
+	if strings.Contains(file.Name(), INVALID_FILE_NAME_CHARACTER) {
+		return "", false
+	}
+
+	return file.Name(), true
+}
+
+func getValue(dir string, fileInfo fs.FileInfo) (string, bool, error) {
+	if fileInfo.Size() == 0 {
+		return "", true, nil
+	}
+
+	file, err := os.Open(dir + "/" + fileInfo.Name())
+
+	if err != nil {
+		return "", false, err
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	scanner.Scan()
+	value := scanner.Text()
+
+	err = scanner.Err()
+	if err != nil {
+		return "", false, err
+	}
+
+	result := strings.TrimRight(value, " \t")
+
+	result = strings.Replace(result, "\x00", "\n", -1)
+
+	return result, false, nil
 }
--- hw08_envdir_tool/executor_test.go
+++ hw08_envdir_tool/executor_test.go
@@ -1,7 +1,84 @@
 package main
 
-import "testing"
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+	//"fmt"
+	"os"
+)
+
+func TestCanSetEnvironmentVariables(t *testing.T) {
+	// backup env variables before the test if any was set
+	backup := backupEnvironment()
+
+	os.Unsetenv("HELLO")
+
+	env := make(Environment)
+	env["HELLO"] = EnvValue{"hello", false}
+
+	setEnvironmentVariables(env)
+
+	value, present := os.LookupEnv("HELLO")
+	require.Equal(t, true, present)
+	require.Equal(t, "hello", value)
+
+	// restore backup variables
+	restoreEnvironmentFromTheBackup(backup)
+}
+
+func TestCanRemoveEnvironmentVariables(t *testing.T) {
+	// backup env variables before the test if any was set
+	backup := backupEnvironment()
+
+	os.Setenv("HELLO", "hello")
+
+	env := make(Environment)
+	env["HELLO"] = EnvValue{"hello", true}
+
+	setEnvironmentVariables(env)
+	_, present := os.LookupEnv("HELLO")
+	require.Equal(t, false, present)
+
+	// restore backup variables
+	restoreEnvironmentFromTheBackup(backup)
+}
 
 func TestRunCmd(t *testing.T) {
-	// Place your code here
+	cmd := make([]string, 2)
+	cmd[0] = "testdata/echo.sh"
+	cmd[1] = "arg1"
+	code := RunCmd(cmd, getAllValues())
+	require.Equal(t, 0, code)
+}
+
+func backupEnvironment() map[string]string {
+	var testEnvNames = []string{"HELLO", "BAR", "FOO", "UNSET", "ADDED", "EMPTY"}
+	result := make(map[string]string)
+
+	for _, name := range testEnvNames {
+		value, present := os.LookupEnv(name)
+		if present == true {
+			result[name] = value
+		}
+	}
+
+	return result
+}
+
+func restoreEnvironmentFromTheBackup(env map[string]string) {
+	for name, value := range env {
+		os.Setenv(name, value)
+	}
+}
+
+func getAllValues() Environment {
+	result := make(Environment)
+	result["HELLO"] = EnvValue{"hello", false}
+	result["BAR"] = EnvValue{"bar", false}
+	result["FOO"] = EnvValue{"foo", false}
+	result["UNSET"] = EnvValue{"unset", false}
+	result["ADDED"] = EnvValue{"added", false}
+	result["EMPTY"] = EnvValue{"empty", false}
+
+	return result
 }
